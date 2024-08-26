54d40a31e4835a432ffc59e925ae51a7ed393c12
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-08-26 09:08:34 +0600

    can get the user data from client

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
@@ -8,6 +10,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 // coffeeMaster
 // CRccCXFOqSvpQzhb
@@ -32,7 +35,13 @@ async function run() {
     // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
+    // authentication related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+    });
 
+    // server related apis
     const coffeeCollection = client
       .db("coffeeMasterDb")
       .collection("coffeeData");
